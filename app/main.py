47cb94eefdf3f8788b4bf8aa07d93790395c7c48
nyashka <sashakulashov@gmail.com>
2025-12-02 16:55:06 +0300

    add search

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Query
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from sqlalchemy import select
+from sqlalchemy import select, or_
 from sqlalchemy.orm import joinedload
 
 from app.api.v1.router import api_router
@@ -10,41 +10,45 @@ from app.models.torrent import Torrent
 
 app = FastAPI(title="Anime Tracker")
 
-# Подключаем статику (CSS, JS, картинки)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-# Настраиваем шаблоны
 templates = Jinja2Templates(directory="app/templates")
-
-# Подключаем API
 app.include_router(api_router, prefix="/api/v1")
 
-# --- Страницы сайта ---
 
 @app.get("/")
-async def index(request: Request):
+async def index(request: Request, q: str = Query(None)):
     """Главная страница: показывает список торрентов"""
     async with AsyncSessionLocal() as db:
-        query = (
-            select(Torrent)
-            .options(joinedload(Torrent.uploader))
-            .order_by(Torrent.id.desc())
-        )
+        query = select(Torrent).options(joinedload(Torrent.uploader))
+
+        if q:
+            search = f"%{q}%"
+            query = query.where(
+                or_(
+                    Torrent.title.ilike(search),
+                    Torrent.description.ilike(search)
+                )
+            )
+
+        query = query.order_by(Torrent.id.desc())
         result = await db.execute(query)
         torrents = result.scalars().all()
 
     return templates.TemplateResponse(
         "index.html",
-        {"request": request, "torrents": torrents}
+        {"request": request, "torrents": torrents, "search_query": q or ""}
     )
+
 
 @app.get("/login")
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
+
 @app.get("/register")
 async def register_page(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
+
 
 @app.get("/upload")
 async def upload_page(request: Request):
